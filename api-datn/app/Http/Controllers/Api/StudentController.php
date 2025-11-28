e67a7a70fbb2c3f3b717b1cbf9d11bf8b99c3422
DinhQuy <hdquy2003@gmail.com>
2025-11-28 23:56:58 +0700

    update inf student (api)

--- api-datn/app/Http/Controllers/Api/StudentController.php
+++ api-datn/app/Http/Controllers/Api/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -41,14 +42,29 @@ class StudentController extends Controller
     public function update(Request $request, Student $student)
     {
         $data = $request->validate([
-            'user_id' => 'sometimes|integer|exists:users,id',
-            'student_code' => 'sometimes|string|max:100',
+            'user_id' => 'string|exists:users,id',
+            'student_id' => 'nullable|string|max:100',
+            'student_code' => 'nullable|string|max:100',
+            'full_name' => 'nullable|string|max:255',
+            'dob' => 'nullable|string|max:100',
+            'phone' => 'nullable|string|max:100',
+            'address' => 'nullable|string|max:255',
             'class_code' => 'nullable|string|max:100',
-            'major_id' => 'nullable|integer',
-            'department_id' => 'nullable|integer',
-            'course_year' => 'nullable|integer'
+            'marjor_id' => 'nullable|integer',
         ]);
-        $student->update($data);
+        $student->student_code = $data['student_code'] ?? $student->student_code;
+        $student->class_code = $data['class_code'] ?? $student->class_code;
+        $student->marjor_id = $data['marjor_id'] ?? $student->marjor_id;
+        $student->save();
+
+        $user = User::find($student->user_id);
+        if ($user) {
+            $user->fullname = $data['full_name'] ?? $user->full_name;
+            $user->dob = $data['dob'] ?? $user->dob;
+            $user->phone = $data['phone'] ?? $user->phone;
+            $user->address = $data['address'] ?? $user->address;
+            $user->save();
+        }
         return response()->json($student->load('user'));
     }
 
